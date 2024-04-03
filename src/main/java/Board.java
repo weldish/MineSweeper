d40d0577702b517e49f2116a280384e22059e3ae
weldish <sc19ws2@leeds.ac.uk>
2024-04-03 21:21:55 +0100

    defined a class for the board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,3 +1,40 @@
+import java.util.Random;
+
 public class Board {
+    private int numRows;
+    private int numCols;
+    private Tile[][] tiles;
+    private int numberOfMines = 10;   //
+    Board(int numRows, int numCols){
+        this.numRows = numRows;
+        this.numCols = numCols;
+        tiles = new Tile[numRows][numCols];   // A 2d-array of tiles
+        initTheBoard();      // initializing the board
+        placeMinesOnBoard();  // placing mines on the board
+
+    }
+
+    private void initTheBoard() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                tiles[i][j] = new Tile( i, j);
+            }
+        }
+    }
+
+    private void placeMinesOnBoard() {
+        Random rand = new Random();
+        int numOfPlacedMines = 0;
+        while (numOfPlacedMines < numberOfMines) {
+            int row = rand.nextInt(numRows);
+            int col = rand.nextInt(numCols);
+            Tile tile = tiles[row][col];
+            if (!tile.isMine) {  // checking incase the tile is a mine
+                tile.isMine = true;
+                numOfPlacedMines++;
+            }
+        }
+    }
+
 
 }
