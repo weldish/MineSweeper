5364fad90bf0e430150618dd3310d6cbd7c41f68
weldish <sc19ws2@leeds.ac.uk>
2024-04-03 22:28:35 +0100

    have added a method for counting adjacent mines

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -11,7 +11,7 @@ public class Board {
         tiles = new Tile[numRows][numCols];   // A 2d-array of tiles
         initTheBoard();      // initializing the board
         placeMinesOnBoard();  // placing mines on the board
-
+        countAdjacentMines(); // after randomly placing the mines, we count adjacent mines for each tile
     }
 
     private void initTheBoard() {
@@ -29,10 +29,40 @@ public class Board {
             int row = rand.nextInt(numRows);
             int col = rand.nextInt(numCols);
             Tile tile = tiles[row][col];
-            if (!tile.isMine) {  // checking incase the tile is a mine
-                tile.isMine = true;
+            if (!tile.isMine()) {  // checking incase the tile is a mine
+                tile.setMine(true);
                 numOfPlacedMines++;
             }
+        }
+    }
+
+    private void countAdjacentMines(){
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                if (!tiles[row][col].isMine()) { // checking if the time is a mine and if it is we don,t need to consider it
+                    int mines = countMines(row, col);
+                    tiles[row][col].setAdjacentMines(mines);
+                }
+            }
+        }
+    }
+
+    private int countMines(int numRows, int numCols){
+        // to be done tommorrow
+    }
+
+
+    public void printBoard(boolean revealMines) {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                Tile tile = tiles[i][j];
+                if (tile.isRevealed() || (revealMines && tile.isMine())) {
+                    System.out.print(tile.isMine() ? "* " : tile.getAdjacentMines() + " ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
         }
     }
 
