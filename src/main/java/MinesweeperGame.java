cbd41ee40a04ace8b3d8b3fd9df26b71a91f025d
weldish <sc19ws2@leeds.ac.uk>
2024-04-03 22:06:19 +0100

    have added some logic into the minesweeperGame class

--- src/main/java/MinesweeperGame.java
+++ src/main/java/MinesweeperGame.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class MinesweeperGame {
+    private Board board;
+    private boolean gameOver;
+
+    public MinesweeperGame(int numRows, int numCols) {
+        this.board = new Board(numRows, numCols);
+
+    }
+
+    public void startGame() {
+        board.printBoard(false);
+    }
+
+    public static void main(String[] args) {
+        MinesweeperGame game = new MinesweeperGame(10, 10);
+        game.startGame();
+    }
 }
