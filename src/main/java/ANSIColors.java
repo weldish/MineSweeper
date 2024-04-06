e1df65dd01ba34779c81ba8c6538cacfe6ad067d
weldish <sc19ws2@leeds.ac.uk>
2024-04-06 14:37:25 +0100

    for printing colored texts and backgrounds

---
+++ src/main/java/ANSIColors.java
@@ -0,0 +1,23 @@
+public class ANSIColors {
+    public static final String RESET = "\u001B[0m";
+
+    public static final String BLACK_TEXT = "\u001B[30m";
+    public static final String RED_TEXT = "\u001B[31m";
+    public static final String GREEN_TEXT = "\u001B[32m";
+    public static final String YELLOW_TEXT = "\u001B[33m";
+    public static final String BLUE_TEXT = "\u001B[34m";
+    public static final String PURPLE_TEXT = "\u001B[35m";
+    public static final String CYAN_TEXT = "\u001B[36m";
+    public static final String WHITE_TEXT = "\u001B[37m";
+
+    // Background colors
+    public static final String BG_BLACK = "\u001B[40m";
+    public static final String BG_RED = "\u001B[41m";
+    public static final String BG_GREEN = "\u001B[42m";
+    public static final String BG_YELLOW = "\u001B[43m";
+    public static final String BG_BLUE = "\u001B[44m";
+    public static final String BG_PURPLE = "\u001B[45m";
+    public static final String BG_CYAN = "\u001B[46m";
+    public static final String BG_WHITE = "\u001B[47m";
+}
+
