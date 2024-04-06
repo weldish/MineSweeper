b38ca5cd46c70f8c1296ea9fc7ba2e2865d519f1
weldish <sc19ws2@leeds.ac.uk>
2024-04-06 14:10:38 +0100

    added a game launcher class

---
+++ src/main/java/MineSweeperLauncher.java
@@ -0,0 +1,6 @@
+public class MineSweeperLauncher {
+    public static void main(String[] args) {
+        MinesweeperGame launchgame = new MinesweeperGame(10, 10);
+        launchgame.startGame();
+    }
+}
